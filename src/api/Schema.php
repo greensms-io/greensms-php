a4117beb4f1b8073fc7fdc578fba54a636b8d585
Manan Jadhav <manan.jadhav@gmail.com>
2020-11-12 19:41:46 +0530

    Added all schemas

--- src/api/Schema.php
+++ src/api/Schema.php
@@ -26,18 +26,77 @@ class Schema {
         ]
       ]
     ];
+
+    return $commonSchema;
   }
 
   static function getSchema() {
+
+    $commonSchema = self::getCommonSchema();
+
     $schema = [
       'account' => [
         'v1' => [
           'token' => [
             'expire' => ['integer', ['min', 0]],
-            ''
           ]
         ]
-      ]
+      ],
+      'call' => $commonSchema,
+      'hlr' => $commonSchema,
+      'general' => [],
+      'whois' => [
+        'v1' => [
+          'lookup' => [
+            'to' => $toSchema
+          ]
+      ],
+      'voice' => [
+        'v1' => [
+          'send' => [
+            'txt' => ['required', ['lengthMin', 1], ['lengthMax', 5], ['regex', '^[0-9]+']],
+            'lang' => [['subset', ['ru', 'en']]]
+          ]
+        ]
+          ],
+          'pay' => [
+            'v1' => [
+              'send' => [
+                'amount' => [ 'required','integer', ['min', 1]],
+                'tag' => ['alphaNum']
+              ],
+
+            ]
+            ],
+            'sms' => [
+              'v1' => [
+                'send' => [
+                  'txt' => ['required', ['lengthMin', 1]],
+                  'from' => ['alphaNum'],
+                  'tag' => ['alphaNum'],
+                ]
+              ]
+                ],
+                'viber' => [
+                  'v1' => [
+                    'send' => [
+                      'txt' => ['required', ['lengthMin', 1]],
+                      'from' => ['alphaNum'],
+                      'cascase' => [['subset', ['sms', 'voice']]],
+                    ]
+                  ]
+                    ],
+                    'social' => [
+                      'v1' => [
+                        'send' => [
+                          'txt' => ['required', ['lengthMin', 1]],
+                          'from' => ['alphaNum'],
+                          'tag' => ['alphaNum'],
+                        ]
+                      ]
+                        ],
+    ],
+
     ];
   }
 }
