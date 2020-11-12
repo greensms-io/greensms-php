929f006b8683abdbb576ddc7299ef200180af711
Manan Jadhav <manan.jadhav@gmail.com>
2020-11-12 21:45:33 +0530

    Module Instance class added

---
+++ src/api/Module.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace GreenSms\Api;
+
+use GreenSms\Utils\Validator;
+
+class Module {
+
+  protected $restClient;
+  protected $moduleSchema;
+  protected $params;
+
+  function __construct($restClient, $moduleSchema, $options) {
+
+    $this->restClient = $restClient;
+    $this->moduleSchema = $moduleSchema;
+    $this->params = $options;
+  }
+
+  function apiFunction($data) {
+    if(!$moduleSchema) {
+      $validationResult = Validator::validate($this->moduleSchema, $data);
+    }
+
+    $requestParams = $this->params;
+    $requestParams['params'] = $data;
+    $response = $this->restClient->request($requestParams);
+    return $response;
+  }
+}
