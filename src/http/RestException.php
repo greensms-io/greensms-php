9d6d59290cca9c46b2e0eae5299c503475e9969d
Manan Jadhav <manan.jadhav@gmail.com>
2020-11-12 18:59:28 +0530

    Added exception class

---
+++ src/http/RestException.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace GreenSms\Http;
+
+use \Exception;
+
+class RestException extends Exception {
+
+
+  protected $errorType = '';
+  protected $params = [];
+
+  public function __constructor($message, $code = 0, $previous = null) {
+
+    $errorMessage = $message;
+    $this->name =  'RestException';
+    parent::__construct($errorMessage, $code, $previous);
+    $errorType = $this->getErrorType($this->code);
+    $this->errorType = $errorType;
+  }
+
+  function setParams($params) {
+    $this->params = $params;
+  }
+
+  function getErrorType($code) {
+    switch ($code) {
+      case 0:
+        return 'AUTH_DECLINED';
+
+      case 1:
+        return 'MISSING_INPUT_PARAM';
+
+      case 2:
+        return 'INVALID_INPUT_PARAM';
+
+      case 404:
+        return 'NOT_FOUND';
+
+      default:
+        return 'INTERNAL_SERVER_ERROR';
+    }
+  }
+}
