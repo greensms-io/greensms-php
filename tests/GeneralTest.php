49b42311d58de35e6fdade716e8e1fbb21a74d58
Manan Jadhav <manan.jadhav@gmail.com>
2020-11-13 19:37:30 +0530

    Added tests for lookup

---
+++ tests/GeneralTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use GreenSms\Tests\Utility;
+use GreenSms\GreenSms;
+
+final class GeneralTest extends TestCase {
+
+  private $utility = null;
+
+  public function setUp() {
+    $this->utility = new Utility();
+  }
+
+  public function testCanFetchLookup() {
+    $response = $this->utility->getInstance()->whois->lookup(['to' => '79260000000']);
+    $this->assertObjectHasAttribute('def', $response);
+    $this->assertObjectHasAttribute('region', $response);
+  }
+
+}
