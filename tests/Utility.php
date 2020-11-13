fa00b05ff5c300806443e03e4291c4f65cb11b47
Manan Jadhav <manan.jadhav@gmail.com>
2020-11-13 22:33:34 +0530

    Lint Fixes

--- tests/Utility.php
+++ tests/Utility.php
@@ -31,4 +31,17 @@ class Utility
     {
         return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoibWFuYW4yNCIsImlhdCI6MTYwMzU2OTgzMCwiaXNzIjoiYXBpLmdyZWVuc21zLnJ1In0.OKiv5itdirS_PuPJj5kgGcR2_9DsC9ALW9c8FvvHSF4';
     }
+
+    public function getInstanceWithEnv()
+    {
+        putenv('GREENSMS_USER=test');
+        putenv('GREENSMS_PASS=test');
+        $envClient = new GreenSms();
+
+        // Unsetting the env variable after use
+        putenv('GREENSMS_USER');
+        putenv('GREENSMS_PASS');
+
+        return $envClient;
+    }
 }
