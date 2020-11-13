3a835a27f6f8fa1859e943998f741e41d84943a1
Manan Jadhav <manan.jadhav@gmail.com>
2020-11-13 19:01:22 +0530

    Added getParams

--- src/http/RestException.php
+++ src/http/RestException.php
@@ -23,6 +23,11 @@ class RestException extends Exception
         $this->params = $params;
     }
 
+    public function getParams()
+    {
+        return $this->params;
+    }
+
     public function __toString() {
       $params = json_encode($this->params);
       return __CLASS__ . ": [{$this->code}]: {$this->message}\n{$this->getTraceAsString()}\n\n{$params}";
