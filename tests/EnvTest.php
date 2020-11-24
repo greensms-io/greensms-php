fc7ce4b484df0035c312eaefa9538cb30fe36cf1
Manan Jadhav <manan.jadhav@gmail.com>
2020-11-25 00:41:06 +0530

    Added setup():void  for PHPUnit8

--- tests/EnvTest.php
+++ tests/EnvTest.php
@@ -9,7 +9,7 @@ final class EnvTest extends TestCase
 {
     private $utility = null;
 
-    public function setUp()
+    public function setUp(): void
     {
         $this->utility = new Utility();
     }
