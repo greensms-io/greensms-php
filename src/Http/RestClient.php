c3d0b4a957f7e0284b4c5c9accdfd7a647ecd569
Pavel Ketov <ketov.pasha@gmail.com>
2022-06-24 20:10:28 +1000

    Data transmission

--- src/Http/RestClient.php
+++ src/Http/RestClient.php
@@ -1,132 +1,128 @@
 <?php
-
 namespace GreenSMS\Http;
 
 use GreenSMS\Utils\Helpers;
 use GreenSMS\Http\RestException;
 
-class RestClient
-{
-    protected $token;
-    protected $defaultData;
-    protected $defaultParams;
-    protected $useCamelCase = false;
-    protected $ch;
+class RestClient {
+	protected $token;
+	protected $defaultData;
+	protected $defaultParams;
+	protected $useCamelCase = false;
+	protected $ch;
 
-    public function __construct($options)
-    {
-        if (is_null($options)) {
-            $options = [];
-        }
+	public function __construct( $options ) {
+		if ( is_null( $options ) ) {
+			$options = [];
+		}
 
-        if (array_key_exists('token', $options)) {
-            $this->token = $options['token'];
-        }
+		if ( array_key_exists( 'token', $options ) ) {
+			$this->token = $options['token'];
+		}
 
-        if (array_key_exists('defaultData', $options)) {
-            $this->defaultData = $options['defaultData'];
-        }
+		if ( array_key_exists( 'defaultData', $options ) ) {
+			$this->defaultData = $options['defaultData'];
+		}
 
-        if (array_key_exists('defaultParams', $options)) {
-            $this->defaultParams = $options['defaultParams'];
-        }
+		if ( array_key_exists( 'defaultParams', $options ) ) {
+			$this->defaultParams = $options['defaultParams'];
+		}
 
-        if (array_key_exists('useCamelCase', $options)) {
-            $this->useCamelCase = $options['useCamelCase'];
-        }
-    }
+		if ( array_key_exists( 'useCamelCase', $options ) ) {
+			$this->useCamelCase = $options['useCamelCase'];
+		}
+	}
 
-    public function request($options)
-    {
-        if (is_null($options)) {
-            $options = [];
-        }
+	public function request( $options ) {
+		if ( is_null( $options ) ) {
+			$options = [];
+		}
 
-        if (!array_key_exists('method', $options)) {
-            throw new Exception('Http Method is required!');
-        }
+		if ( ! array_key_exists( 'method', $options ) ) {
+			throw new Exception( 'Http Method is required!' );
+		}
 
-        if (!array_key_exists('url', $options)) {
-            throw new Exception('URL is required!');
-        }
+		if ( ! array_key_exists( 'url', $options ) ) {
+			throw new Exception( 'URL is required!' );
+		}
 
-        $this->ch = curl_init();
+		$this->ch = curl_init();
 
-        if (strtolower($options['method']) === 'post') {
-            curl_setopt($this->ch, CURLOPT_POST, 1);
-        } else {
-            curl_setopt($this->ch, CURLOPT_POST, 0);
-        }
+		if ( strtolower( $options['method'] ) == 'post' ) {
+			curl_setopt( $this->ch, CURLOPT_POST, 1 );
+		} else {
+			curl_setopt( $this->ch, CURLOPT_POST, 0 );
+		}
 
-        $headers = [
-            'Content-Type' => 'application/json'
-        ];
+		$params = [];
+		if ( ! empty( $this->defaultParams ) ) {
+			$params = $this->defaultParams;
+		}
+		if ( array_key_exists( 'params', $options ) ) {
+			$params = array_merge( $params, $options['params'] );
+		}
 
+		$url = $options['url'];
+		$params_str = http_build_query( $params );
 
-        if ($this->token) {
-            $headers['Authorization'] = 'Bearer '. $this->token;
-        }
+		if ( strlen( $params_str ) > 0 ) {
+			if ( strtolower( $options['method'] ) === 'post' ) {
+				curl_setopt( $this->ch, CURLOPT_POSTFIELDS, json_encode( $params ) );
+			} else {
+				$url .= ( parse_url( $url, PHP_URL_QUERY ) ? '&' : '?' ) . $params_str;
+			}
+		}
 
-        if (array_key_exists('headers', $options)) {
-            $headers = array_merge($headers, $options['headers']);
-        }
+		curl_setopt( $this->ch, CURLOPT_URL, $url );
+		curl_setopt( $this->ch, CURLOPT_RETURNTRANSFER, true );
+		curl_setopt( $this->ch, CURLOPT_HEADER, 0 );
+		curl_setopt( $this->ch, CURLOPT_SSL_VERIFYHOST, 0 );
+		curl_setopt( $this->ch, CURLOPT_SSL_VERIFYPEER, 0 );
 
-        if (!empty($headers)) {
-            $headers_arr = [];
-            foreach ($headers as $k => $v) {
-                if (is_array($v)) {
-                    foreach ($v as $val) {
-                        $headers_arr[] = "$k: $val";
-                    }
-                } else {
-                    $headers_arr[] = "$k: $v";
-                }
-            }
-            curl_setopt($this->ch, CURLOPT_HTTPHEADER, $headers_arr);
-        }
+		$headers = [
+			'Content-Type' => 'application/json'
+		];
 
+		if ( $this->token ) {
+			$headers['Authorization'] = 'Bearer ' . $this->token;
+		}
 
-        $params_str = "";
-        $params = [];
-        if (!empty($this->defaultParams)) {
-            $params = $this->defaultParams;
-        }
-        if (array_key_exists('params', $options)) {
-            $params = array_merge($params, $options['params']);
-        }
+		if ( array_key_exists( 'headers', $options ) ) {
+			$headers = array_merge( $headers, $options['headers'] );
+		}
 
-        $url = $options['url'];
-        $params_str = http_build_query($params);
+		if ( ! empty( $headers ) ) {
+			$headers_arr = [];
+			foreach ( $headers as $k => $v ) {
+				if ( is_array( $v ) ) {
+					foreach ( $v as $val ) {
+						$headers_arr[] = "$k: $val";
+					}
+				} else {
+					$headers_arr[] = "$k: $v";
+				}
+			}
+			curl_setopt( $this->ch, CURLOPT_HTTPHEADER, $headers_arr );
+		}
 
-        if (strlen($params_str) > 0) {
-            $url .= (parse_url($url, PHP_URL_QUERY) ? '&' : '?') . $params_str;
-        }
+		$apiResult = curl_exec( $this->ch );
+		$response = json_decode( $apiResult, true );
 
-        curl_setopt($this->ch, CURLOPT_URL, $url);
-        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($this->ch, CURLOPT_HEADER, 0);
-        curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, 0);
+		if ( curl_errno( $this->ch ) ) {
+			$error = curl_error( $this->ch );
+			$response = new RestException( $error->message, $error->code, $error->previous );
+		}
 
+		curl_close( $this->ch );
 
-        $apiResult = curl_exec($this->ch);
-        $response = json_decode($apiResult, true);
+		if ( array_key_exists( 'error', $response ) ) {
+			throw new RestException( $response['error'], $response['code'] );
+		}
 
-        if (curl_errno($this->ch)) {
-            $error = curl_error($this->ch);
-            $response = new RestException($error->message, $error->code, $error->previous);
-        }
+		if ( $this->useCamelCase ) {
+			$response = Helpers::camelizeKeys( $response );
+		}
 
-        curl_close($this->ch);
-
-        if (array_key_exists('error', $response)) {
-            throw new RestException($response['error'], $response['code']);
-        }
-
-        if ($this->useCamelCase) {
-            $response = Helpers::camelizeKeys($response);
-        }
-
-        return (object)$response;
-    }
+		return (object) $response;
+	}
 }
