5b386200a8856c29b54f9862ec831d5e66223e39
Manan Jadhav <manan.jadhav@gmail.com>
2023-04-03 00:21:39 +0530

    fix: added vk schmea

--- src/Api/Modules.php
+++ src/Api/Modules.php
@@ -83,6 +83,21 @@ class Modules
                   ]
               ]
           ],
+          'vk' => [
+            'schema' => $schema['vk'],
+            'versions' => [
+                'v1' => [
+                    'send' => [
+                        'args' => ['params'],
+                        'method' => 'POST',
+                    ],
+                    'status' => [
+                        'args' => ['params'],
+                        'method' => 'GET',
+                    ],
+                ]
+            ]
+        ],
           'pay' => [
               'schema' => $schema['pay'],
               'versions' => [
