0ad87a73550f497dd5e65931d21278e5e61fb887
John Doe <johndoe@example.com>
2024-08-08 15:19:42 +0300

    Added environment variable `GREENSMS_BASE_URL`

--- tests/EnvTest.php
+++ tests/EnvTest.php
@@ -1,7 +1,5 @@
 <?php
-
-
-
+use GreenSMS\Http\RestException;
 use GreenSMS\Tests\Utility;
 use GreenSMS\GreenSMS;
 use GreenSMS\Tests\TestCase;
@@ -20,5 +18,17 @@ final class EnvTest extends TestCase
         $client = $this->utility->getInstanceWithEnv();
         $response = $client->account->balance();
         $this->assertObjectHasAttribute('balance', $response);
+    }
+
+    public function testBaseUrl()
+    {
+        $subDomain = (string) bin2hex(random_bytes(10));
+        putenv('GREENSMS_BASE_URL=https://'. $subDomain .'.greensms.io:');
+        $client = $this->utility->getInstance();
+        $response = $client->account->balance();
+        putenv('GREENSMS_BASE_URL');
+
+        $this->assertInstanceOf(RestException::class, $response);
+        $this->assertStringContainsString($subDomain, $response->getMessage());
     }
 }
